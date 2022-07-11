91dd0194f5bdbb8787a81de2ece0efb2301cdf6a
addi00000 <addison@othius.com>
2022-07-10 23:19:19 -0400

    automatically generate a markdown from stealer json data

---
+++ STEALERS.md
@@ -0,0 +1,65 @@
+# Stealers
+## Bby Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    Has resale program
+    ↳ 1337 stealer
+        Closed source
+        Paid
+        Coded with JavaScript 
+## Pirate Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    No resale program
+## Redfinger Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    No resale program
+## Mitsu Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    No resale program
+## Rustler Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    No resale program
+## Lny Stealer
+    Closed source
+    Paid
+    Coded with JavaScript 
+    No resale program
+## Hazard Token Grabber V2
+    Open source
+    Free
+    Coded with Python 
+    No resale program
+## Oracle Stealer
+    Closed source
+    Paid
+    Coded with Python 
+    No resale program
+## Empyrean
+    Open source
+    Free
+    Coded with Python 
+    No resale program
+## Pegasus
+    Open source
+    Free
+    Coded with Python 
+    No resale program
+## Mercurial Grabber
+    Open source
+    Free
+    Coded with C# 
+    No resale program
+## Threat Stealer
+    Closed source
+    Paid
+    Coded with Python 
+    No resale program
---
+++ generate_stealers-md.py
@@ -0,0 +1,36 @@
+import json
+
+__LANGS__ = {
+    'py' : 'Python',
+    'js' : 'JavaScript',
+    'cs' : 'C#',
+    'java' : 'Java',
+    'c' : 'C',
+    'cpp' : 'C++',
+    'php' : 'PHP',
+    'rb' : 'Ruby',
+    'go' : 'Go',
+    'swift' : 'Swift',
+    'rust' : 'Rust',
+}
+
+with open('stealers.json', 'r', encoding='utf-8') as f:
+    stealers = json.load(f)
+    
+with open('STEALERS.md', 'w', encoding='utf-8') as f:
+    f.write('# Stealers\n')
+    for stealer in stealers:
+        stealer = stealers[stealer]
+        f.write(f'## {stealer["name"].title()}\n')
+        f.write(f"    {'Open' if stealer['open source'] else 'Closed'} source\n")
+        f.write(f"    {'Free' if not stealer['paid'] else 'Paid'}\n")
+        f.write(f"    Coded with {__LANGS__[stealer['language']]} \n")
+        f.write(f"    {'Has' if stealer['resellable'] else 'No'} resale program\n")
+        if stealer['resellable']:
+            for reseller in stealer['resellers']:
+                reseller = stealer['resellers'][reseller]
+                f.write(f"    ↳ {reseller['name']}\n")
+                f.write(f"        {'Open' if reseller['open source'] else 'Closed'} source\n")
+                f.write(f"        {'Free' if not reseller['paid'] else 'Paid'}\n")
+                f.write(f"        Coded with {__LANGS__[reseller['language']]} \n")
+    
