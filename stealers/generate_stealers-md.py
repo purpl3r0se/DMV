03be0bb44b75ae134b1bb5b26c283c1ed0025af5
purpl3r0se <purpl3r0se@protonmail.com>
2022-12-22 23:59:35 +0200

    Add files via upload

--- stealers/generate_stealers-md.py
+++ stealers/generate_stealers-md.py
@@ -1,4 +1,7 @@
 import json
+import os
+
+use_full_path = bool(os.getenv("full_path"))
 
 __LANGS__ = {
     'py': 'Python',
@@ -12,10 +15,23 @@ __LANGS__ = {
     'rs': 'Rust'
 }
 
-with open('stealers.json', 'r', encoding='utf-8') as f:
+def get_maintenace_status(stealer: dict) -> str:
+    maintenance_status = ""
+    maintained = stealer.get('maintained', "unknown")
+    if maintained == "no":
+        maintenance_status = "❌ Not maintained"
+    elif maintained == "somewhat":
+        maintenance_status = "🟡 Somewhat maintained"
+    elif maintained == "unknown":
+        maintenance_status = "🤷‍♂️ Maintenance status unknown"
+    elif maintained == "yes":
+        maintenance_status = "✅ Maintained"
+    return maintenance_status
+
+with open(f'{"./stealers/" if use_full_path else ""}stealers.json', 'r', encoding='utf-8') as f:
     stealers = json.load(f)
 
-with open('STEALERS.md', 'w', encoding='utf-8') as f:
+with open(f'{"./stealers/" if use_full_path else ""}STEALERS.md', 'w', encoding='utf-8') as f:
     f.write('# Stealers\n')
     f.write('## Breakdown\n')
 
@@ -32,36 +48,30 @@ with open('STEALERS.md', 'w', encoding='utf-8') as f:
         f.write(f"- {__LANGS__[lang]}\n")
         f.write(f"    - {langs.count(__LANGS__[lang])} occurences\n")
 
+    f.write('## List\n')
+
     for stealer in stealers:
         stealer = stealers[stealer]
-        f.write(
-            f'### {stealer["name"].title()} | Creator: {"Unknown" if stealer["owner"] == "" else stealer["owner"]}\n')
-        f.write(
-            f"    {'🔓 Open' if stealer['open source'] else '🔒 Closed'} source\n")
-        f.write(f"    {'🆓 Free' if not stealer['paid'] else '💰 Paid'}\n")
+        f.write(f'### {stealer["name"].title()} | Creator: {"Unknown" if stealer["owner"] == "" else stealer["owner"]}\n')
+        f.write(f"    {'🔓 Open' if stealer['open source'] else '🔒 Closed'} source\n")
+        f.write(f"    {'💰 Paid' if stealer['paid'] else '🆓 Free'}\n")
         f.write(f"    💻 Coded with {__LANGS__[stealer['language']]} \n")
         if stealer['resellable']:
-            f.write(
-                "    💸 Has resale program\n")
-        if 'notmaintained' in stealer and stealer['notmaintained'] is True:
-            f.write(
-                "    ❌ Not Maintained\n")
+            f.write("    💸 Has resale program\n")
+
+        maintenance_status = get_maintenace_status(stealer)
+        f.write(f"    {maintenance_status}\n")
+
         if 'telegram' in stealer:
-            f.write(
-                f"    💬 Telegram: t.me/{stealer['telegram']}\n")
+            f.write(f"    💬 Telegram: t.me/{stealer['telegram']}\n")
         if stealer['resellable']:
             for reseller in stealer['resellers']:
                 reseller = stealer['resellers'][reseller]
                 f.write(f"    ↳ {reseller['name']}\n")
-                f.write(
-                    f"        {'🔓 Open' if reseller['open source'] else '🔒 Closed'} source\n")
-                f.write(
-                    f"        {'🆓 Free' if not reseller['paid'] else '💰 Paid'}\n")
-                f.write(
-                    f"        💻 Coded with {__LANGS__[reseller['language']]} \n")
-                if 'notmaintained' in reseller and reseller['notmaintained'] is True or 'notmaintained' in stealer and stealer['notmaintained'] is True:
-                    f.write(
-                        "        ❌ Not Maintained\n")
+                f.write(f"        {'🔓 Open' if reseller['open source'] else '🔒 Closed'} source\n")
+                f.write(f"        {'💰 Paid' if reseller['paid'] else '🆓 Free'}\n")
+                f.write(f"        💻 Coded with {__LANGS__[reseller['language']]} \n")
+                maintenance_status = get_maintenace_status(reseller)
+                f.write(f"        {maintenance_status}\n")
                 if 'telegram' in reseller:
-                    f.write(
-                        f"        💬 Telegram: t.me/{stealer['telegram']}\n")
+                    f.write(f"        💬 Telegram: t.me/{stealer['telegram']}\n")
